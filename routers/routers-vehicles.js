40e2b0f208cf980988a194debd674634b93cae93
DavidStephenss <dvdstephensd@gmail.com>
2020-08-16 11:46:01 -0500

    vehicles done

--- routers/routers-vehicles.js
+++ routers/routers-vehicles.js
@@ -3,10 +3,9 @@ const router = express.Router()
 const vehicleController = require('../controllers/controllers-vehicles')
 
 router.get('/vehicles', vehicleController.listVehicles)
-// router.post('/vehicles', vehicleController.createVehicle)
-// router.get('/vehicles', vehicleController.getThemVehicles)
-// router.get('/vehicles:id', vehicleController.getOneVehicle)
-// router.put('/vehicles:id', vehicleController.updateVehicle)
-// router.delete('/vehicles:id', vehicleController.deleteVehicle)
+router.post('/vehicles', vehicleController.createVehicle)
+router.get('/vehicles', vehicleController.getOneVehicle)
+router.put('/vehicles:id', vehicleController.updateVehicle)
+router.delete('/vehicles:id', vehicleController.deleteVehicle)
 
 module.exports = router
